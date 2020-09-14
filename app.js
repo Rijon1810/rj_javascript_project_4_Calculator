f938c0f5dd899c6d4e87efec4aef6414ab490d9d
Rokibul Hasan Rijon <44047172+Rijon1810@users.noreply.github.com>
2020-09-14 18:30:09 +0600

    forget to give break function

--- app.js
+++ app.js
@@ -33,21 +33,24 @@ class Calculator {
         const prev = parseFloat(this.previousOperand);
         const current = parseFloat(this.currentOperand);
         if (isNaN(prev) || isNaN(current)) return
-        switch (this.operation) {
+       switch (this.operation) {
             case '+':
                 computation = prev + current;
                 break;
             case '-':
                 computation = prev - current;
+                break;
             case '*':
                 computation = prev * current;
                 break;
+                
             case '÷':
                 computation = prev / current;
+                break;
 
-            default:
-                return
-
+             default :
+             return 
+            
         }
         this.currentOperand = computation;
         this.operation = undefined;
